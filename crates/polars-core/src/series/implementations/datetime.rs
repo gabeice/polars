0e4a2385c2482d2fbc7eb2e1455426e821569747
Gabriel McDonald <gmcdonald@ycharts.com>
2025-02-20 14:06:52 -0500

    top_k performance improvement initial spike - series

--- crates/polars-core/src/series/implementations/datetime.rs
+++ crates/polars-core/src/series/implementations/datetime.rs
@@ -290,6 +290,10 @@ impl SeriesTrait for SeriesWrap<DatetimeChunked> {
         self.0.get_any_value_unchecked(index)
     }
 
+    fn top_k(&self, k: usize, descending: bool) -> PolarsResult<Series> {
+        Ok(self.0.top_k(k, descending).into_series())
+    }
+
     fn sort_with(&self, options: SortOptions) -> PolarsResult<Series> {
         Ok(self
             .0
